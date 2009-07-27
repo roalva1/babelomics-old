1577c460f631cbb00eee79d805b0531cfdc67aab
lpulido <lpulido@cipf.es>
2009-07-27 12:44:57 +0200

    no

---
+++ src/main/java/org/bioinfo/babelomics/tools/functional/Marmite.java
@@ -0,0 +1,66 @@
+package org.bioinfo.babelomics.tools.functional;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+import org.bioinfo.babelomics.tools.BabelomicsTool;
+import org.bioinfo.data.dataset.Dataset;
+import org.bioinfo.tool.OptionFactory;
+
+public class Marmite extends BabelomicsTool {
+
+
+	public Marmite(String[] args) {
+		super(args);
+		initOptions();
+	}
+
+	@Override
+	public void initOptions() {
+		options.addOption(OptionFactory.createOption("dataset", "the data"));
+		options.addOption(OptionFactory.createOption("bioentity-name", "possibilities: wordRoots,diseaseAssociated,chemicalProducts"));
+		options.addOption(OptionFactory.createOption("bioentity-score-filter", "Minimum number of genes with a score (0-10000)"));
+		options.addOption(OptionFactory.createOption("bioentity-number-filter", "Number of bio-entities in results (0-10000)"));
+		options.addOption(OptionFactory.createOption("gene-name-list", "gene name in list", false,false));
+		options.addOption(OptionFactory.createOption("partition-number", "Number of partitions "));
+		options.addOption(OptionFactory.createOption("significance", "p-value for statistical significance"));
+		options.addOption(OptionFactory.createOption("sort", "Sort list"));
+
+	}
+
+	@Override
+	public void execute() {
+		try {
+			CommandLine cmd = parse(args);
+
+			Dataset dataset = new Dataset(new File(cmd.getOptionValue("dataset")));
+			String bioEntityName = cmd.getOptionValue("bioentity-name");
+			String bioentityScoreFilter = cmd.getOptionValue("bioentity-score-filter");
+			String bioentityNumberFilter = cmd.getOptionValue("bioentity-mumber-filter");			
+			String geneNameList = cmd.getOptionValue("gene-name-list", null);
+			String partitionNumber = cmd.getOptionValue("partition-number");	
+			double significance = Double.parseDouble(cmd.getOptionValue("significance", "0.05"));
+			String sort = cmd.getOptionValue("sort");
+			System.out.println(dataset.toString()+"\n");
+			
+			executeMarmite(dataset, bioEntityName, bioentityScoreFilter, bioentityNumberFilter,geneNameList, partitionNumber, significance,  sort);
+			
+		} catch (ParseException e) {
+			logger.error("Error parsing command line", e.toString());
+			System.out.println("\n");
+			printUsage();
+		} catch (IOException e) {
+			logger.error("Error opening the dataset", e.toString());
+		}
+		
+		
+		
+	}
+	
+	private void executeMarmite(Dataset dataset, String bioEntityName, String bioentityScoreFilter, String bioentityNumberFilter,String geneNameList, String partitionNumber, String significance, String sort) {
+		logger.info("executing svm, not implemented yet");
+	}
+	
+}
