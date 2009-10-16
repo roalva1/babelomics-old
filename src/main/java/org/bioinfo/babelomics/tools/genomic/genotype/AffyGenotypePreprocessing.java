4a072f53eee0cb4581a988f02b6f182eda619afa
imedina <imedina@cipf.es>
2009-10-16 13:38:20 +0200

    Genotype work done

--- src/main/java/org/bioinfo/babelomics/tools/genomic/genotype/AffyGenotypePreprocessing.java
+++ src/main/java/org/bioinfo/babelomics/tools/genomic/genotype/AffyGenotypePreprocessing.java
@@ -8,6 +8,7 @@ import org.bioinfo.babelomics.tools.BabelomicsTool;
 import org.bioinfo.commons.exec.Command;
 import org.bioinfo.commons.exec.SingleProcess;
 import org.bioinfo.commons.io.utils.FileUtils;
+import org.bioinfo.commons.utils.StringUtils;
 import org.bioinfo.tool.OptionFactory;
 
 public class AffyGenotypePreprocessing extends BabelomicsTool {
@@ -31,10 +32,10 @@ public class AffyGenotypePreprocessing extends BabelomicsTool {
 
 	@Override
 	protected void execute() {
-		File dataDirFile = new File(commandLine.getOptionValue("data-dir"));
-		if(dataDirFile == null || !dataDirFile.exists()) {
-			abort("", "", "", "");
-		}
+//		File dataDirFile = new File(commandLine.getOptionValue("data-dir"));
+//		if(dataDirFile == null || !dataDirFile.exists()) {
+//			abort("", "", "", "");
+//		}
 
 		if(!commandLine.hasOption("calls") || !commandLine.hasOption("chp-to-ped-map")) {
 			printUsage("./babelomics.sh");
@@ -50,19 +51,18 @@ public class AffyGenotypePreprocessing extends BabelomicsTool {
 			}
 			File cdfFile = new File(commandLine.getOptionValue("cdf-file"));
 			File chrXSnpsFile = new File(commandLine.getOptionValue("chrX-snps"));
-
+			File dataDirFile = new File(commandLine.getOptionValue("data-dir"));
 			try {
-				FileUtils.checkFile(cdfFile);
-				FileUtils.checkFile(chrXSnpsFile);
 				if(!commandLine.getOptionValue("chip-type").equalsIgnoreCase("500k") || !commandLine.getOptionValue("chip-type").equalsIgnoreCase("6.0")) {
 					abort("", "", "", "");
 					logger.error("No valid chip type: 500k or 6.0");
 				}
-
-				AffyGenotypeUtils.affyGenotypeNormalization(babelomicsHomePath+"/bin/apt/apt-probeset-genotype", commandLine.getOptionValue("chip-type"), cdfFile.getAbsolutePath(), chrXSnpsFile.getAbsolutePath());
+				
+				AffyGenotypeUtils.affyGenotypeNormalization(babelomicsHomePath+"/bin/apt/apt-probeset-genotype", commandLine.getOptionValue("chip-type"), cdfFile.getAbsolutePath(), chrXSnpsFile.getAbsolutePath(), outdir, dataDirFile.getAbsolutePath());
 				
 			}catch (IOException e) {
-				
+				abort("", "", "", "");
+				logger.error("Error in calls", StringUtils.getStackTrace(e));
 			}
 			// update dataDirFile with chp dir
 			dataDirFile = new File(outdir+"/chp-files");
@@ -74,54 +74,6 @@ public class AffyGenotypePreprocessing extends BabelomicsTool {
 		}
 
 
-		StringBuilder aptCommandLine = new StringBuilder();
-		aptCommandLine.append("apt-probeset-genotype ");
-
-		// cdf file
-		File cdfFile = new File(commandLine.getOptionValue("cdf-file"));
-		if(!cdfFile.exists()) {
-			abort("", "", "", "");
-		}
-		logger.debug("cdf-file valid");
-		aptCommandLine.append(" -c " + cdfFile.getAbsolutePath());
-
-		// chrX-snp file
-		File chrXSnpsFile = new File(commandLine.getOptionValue("chrX-snps"));
-		// to finish
-		if(!chrXSnpsFile.exists()) {
-			abort("", "", "", "");
-		}
-		logger.debug("chrX-snps valid");
-		aptCommandLine.append(" --chrX-snps " + chrXSnpsFile.getAbsolutePath());
-
-		if(commandLine.getOptionValue("chip-type").equalsIgnoreCase("6.0")) {
-			aptCommandLine.append(" -a birdseed ");
-		}else {
-			if(commandLine.getOptionValue("chip-type").equalsIgnoreCase("500k")) {
-
-			}else {
-				logger.error("No valid chip type: 500k or 6.0");
-			}
-		}
-
-		aptCommandLine.append(" --cc-chp-output ");
-		aptCommandLine.append(" -o " + new File(outdir).getAbsolutePath());
-
-		File celFiles = new File(commandLine.getOptionValue("data-dir"));
-		File[] files = FileUtils.listFiles(celFiles, ".+.cel", true);
-		if(files.length < 2) {
-			// print error
-			logger.error("data-dir does not exist");
-		}
-		for(int i=0; i<files.length; i++) {
-			aptCommandLine.append(" " + files[i].getAbsolutePath());
-		}
-
-		Command aptCommand = new Command(aptCommandLine.toString());
-		SingleProcess sp = new SingleProcess(aptCommand);
-		logger.debug("executing... " + aptCommand.getCommandLine());
-		sp.runSync();
-		logger.debug("done!");
 	}
 
 }
