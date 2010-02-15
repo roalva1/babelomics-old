6fc9cbedd7110acfaf444ceafc9db438169bded4
imedina <imedina@cipf.es>
2010-02-15 12:26:19 +0100

    Genotype stratification: basic work done

--- src/main/java/org/bioinfo/babelomics/tools/genomic/genotype/StratificationTool.java
+++ src/main/java/org/bioinfo/babelomics/tools/genomic/genotype/StratificationTool.java
@@ -13,7 +13,7 @@ public class StratificationTool extends GenotypeAnalysisTool {
 
 	@Override
 	public void initOptions() {
-		options.addOption(OptionFactory.createOption("ibs-cluster", "To perform complete linkage clustering of individuals on the basis of autosomal genome-wide SNP data", false, false));
+		options.addOption(OptionFactory.createOption("ibs-cluster", "To perform complete linkage clustering of individuals on the basis of autosomal genome-wide SNP data", true, false));
 		options.addOption(OptionFactory.createOption("ibs-test", "To test whether or not there are group differences in this metric, with respect to a binary phenotype", false, false));
 		options.addOption(OptionFactory.createOption("ppc", "Pairwise population concordance (PPC). To only merge clusters that do not contain individuals differing at a certain p-value, by default 0.0001", false, true));
 		options.addOption(OptionFactory.createOption("mc", "Maximum cluster size, not applied by default", false, true));
