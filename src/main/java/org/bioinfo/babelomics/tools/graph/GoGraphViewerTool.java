6b731b0c8b7adbd81c2108489db9aa2572349e33
Josete <jose@josete-desktop.(none)>
2010-03-07 19:06:16 +0100

    filter descriptor

--- src/main/java/org/bioinfo/babelomics/tools/graph/GoGraphViewerTool.java
+++ src/main/java/org/bioinfo/babelomics/tools/graph/GoGraphViewerTool.java
@@ -127,7 +127,7 @@ public class GoGraphViewerTool  extends BabelomicsTool{
 		result.addOutputItem(new Item("imagePng", PREFIX + "_graphimage.png", "Graph as PNG", Item.TYPE.FILE, "Images"));
 		result.addOutputItem(new Item("imageSVG", PREFIX + "_graphimage.svg", "Graph as SVG", Item.TYPE.FILE, "Images"));
 		// jnlp link
-		result.addOutputItem(new Item("jnlplink",config.get("JNLP_DOWNLOADER_HOST_NAME")+"b2g.jnlp", "Link to interactive GoGraphViz applicacion (open with Java-Web-Start)", Item.TYPE.LINK, "Images"));
+		result.addOutputItem(new Item("jnlplink",config.get("JNLP_DOWNLOADER_HOST_NAME")+"b2g.jnlp", "Link to interactive GoGraphViz applicacion (open with Java-Web-Start)", Item.TYPE.LINK, Arrays.asList("INCLUDE_REFS"), new HashMap<String, String>(),"Images"));
 		// graph as text		
 		result.addOutputItem(new Item("graphTxt", PREFIX + "_graph.txt", "Graph as textual representation",  Item.TYPE.FILE, "Graph as text"));
 	}
