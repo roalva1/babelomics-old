861949eb3f90ec73324d3c24a7a98b2c054dd21b
ralonso <ralonso@cipf.es>
2010-07-14 15:34:12 +0200

    Snow2 class created

---
+++ src/main/java/org/bioinfo/babelomics/tools/interactome/Snow2.java
@@ -0,0 +1,17 @@
+package org.bioinfo.babelomics.tools.interactome;
+
+import org.bioinfo.babelomics.tools.BabelomicsTool;
+
+public class Snow2 extends BabelomicsTool {
+
+	@Override
+	public void initOptions() {
+		
+	}
+
+	@Override
+	protected void execute() {
+		
+	}
+
+}
